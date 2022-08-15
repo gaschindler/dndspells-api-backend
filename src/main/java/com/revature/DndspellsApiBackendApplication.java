90230fce23f2f6233fba57cdd2567189fbbf5c54
gaschindler <gaschindler2018@gmail.com>
2022-08-15 12:44:40 -0400

    initial commit

---
+++ src/main/java/com/revature/DndspellsApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndspellsApiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndspellsApiBackendApplication.class, args);
+	}
+
+}
