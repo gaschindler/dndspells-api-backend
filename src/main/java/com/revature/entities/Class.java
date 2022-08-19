a2bbe3f43d4c93a3f965b2676070043ff1f3293b
gaschindler <gaschindler2018@gmail.com>
2022-08-19 15:56:39 -0400

    added annotations to class and school entities to prevent circular redundancy

--- src/main/java/com/revature/entities/Class.java
+++ src/main/java/com/revature/entities/Class.java
@@ -11,16 +11,21 @@ import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name="classes")
 @Data @AllArgsConstructor @NoArgsConstructor
 @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+@EqualsAndHashCode(exclude= {"spells"}) @ToString(exclude= {"spells"})
+@JsonIgnoreProperties("spells")
 public class Class {
 
 	@Id
