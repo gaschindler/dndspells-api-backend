85e5e9c06c6afaa47ff9119e582fdd62825191bd
gaschindler <gaschindler2018@gmail.com>
2022-08-22 14:34:23 -0400

    added more methods to spell service

--- src/main/java/com/revature/data/SpellRepository.java
+++ src/main/java/com/revature/data/SpellRepository.java
@@ -1,6 +1,7 @@
 package com.revature.data;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,5 +14,7 @@ public interface SpellRepository extends JpaRepository<Spell, Integer> {
 
 	@Query("select s.id, s.name from Spell s")
 	List<Spell> findAll();
+	
+	Optional<Spell> findByName(String name);
 	
 }
--- src/main/java/com/revature/service/SpellService.java
+++ src/main/java/com/revature/service/SpellService.java
@@ -2,6 +2,7 @@ package com.revature.service;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.data.SpellRepository;
 import com.revature.entities.Spell;
+import com.revature.exceptions.SpellNotFoundException;
 import com.revature.smallentities.SmallSpell;
 
 @Service
@@ -39,5 +41,48 @@ public class SpellService {
 		
 		return returnSpells;
 	}
+	
+	@Transactional(readOnly=true)
+	public Spell getById(int id) throws SpellNotFoundException {
+		// id must be greater than 0
+		if (id <= 0) {
+			log.warn("Spell ID given was invalid. ID passed was: {}. Returning a 400 error", id);
+			throw new SpellNotFoundException("Invalid spell ID. ID must be greater than 0.");
+		}
+		
+		Optional<Spell> returnSpell = spellRepo.findById(id);
+		
+		if(!returnSpell.isPresent()) {
+			log.warn("ID given not associated with a spell in database. ID passed was: {}. Returning a 400 error", id);
+			throw new SpellNotFoundException("Invalid spell ID. ID not in database.");
+		}
+		
+		log.info("Spell with the ID: {} located in database. Returning this spell");
+		return returnSpell.get();
+	}
+	
+	@Transactional(readOnly=true)
+	public Spell getByName(String name) throws SpellNotFoundException {
+		// make name lower case to match database
+		name = name.toLowerCase();
+		
+		// name must not be blank
+		if (name.equals("")) {
+			log.warn("Spell name given was invalid. Name passed was: \"{}\". Returning a 400 error", name);
+			throw new SpellNotFoundException("Invalid spell name. Name must not be blank.");
+		}
+		
+		Optional<Spell> returnSpell = spellRepo.findByName(name);
+		
+		if (!returnSpell.isPresent()) {
+			log.warn("Name given not associated with a spell in database. Name passed was: \"{}\". Returning a 400 error", name);
+			throw new SpellNotFoundException("Invalid spell name. Name not in database.");
+		}
+		
+		log.info("Spell with the name: {} located in database. Returning this spell");
+		return returnSpell.get();
+	}
+	
+	
 	
 }
